ee688f1ed97baef1a95194ec4ac7056de3587292
alexander tejeda <alexander.tejeda@live.se>
2024-11-05 18:00:34 +0100

    Tog bort en extra import av testimonialssection

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './assets/css/Test.css';
-import Testimonialssection from './components/Testimonialssection';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Logosection from './components/Logosection';
